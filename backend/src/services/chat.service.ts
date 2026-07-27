2d8ee86b5c047076951351c147b1ef02ebaa9319
Kxnwn <kenbadiola1@gmail.com>
2026-07-27 21:24:31 +0800

    Change Model

--- backend/src/services/chat.service.ts
+++ backend/src/services/chat.service.ts
@@ -57,7 +57,7 @@ export const chatService = async (
     
 
     const response = await ai.models.generateContent({
-        model: "gemini-3.5-flash",
+        model: "gemini-3.6-flash",
         contents: prompt,
     })
 
--- backend/src/services/memory-extractor.service.ts
+++ backend/src/services/memory-extractor.service.ts
@@ -81,7 +81,7 @@ ${message}
 `;
 
  const response = await ai.models.generateContent({
-        model: "gemini-3.5-flash",
+        model: "gemini-3.6-flash",
         contents: prompt,
     })
 
